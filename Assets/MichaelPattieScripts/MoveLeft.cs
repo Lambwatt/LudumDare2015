f8c72cb58775c34b7f91640dda61d1f1a4db0356
Michael Pattie <mjpattie@gmail.com>
2015-12-11 20:12:50 -0800

    Added test scene, test player drifting right, and slowly scrolling background.

---
+++ Assets/MichaelPattieScripts/MoveLeft.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeft : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position+=new Vector3(Time.deltaTime*speed, 0, 0);
+	}
+}
