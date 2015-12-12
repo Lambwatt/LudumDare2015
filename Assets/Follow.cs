df82268957f943ecedf868397212a0fbb9e6e89e
Michael Pattie <mjpattie@gmail.com>
2015-12-11 21:40:06 -0800

    working on stuff. won't push till fixed.

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -4,9 +4,27 @@ using System.Collections;
 public class Follow : MonoBehaviour {
 
 	public Transform target;
+	public float  distance;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position+=new Vector3(target.position.x-transform.position.x, 0, 0);
+		transform.position = target.position + new Vector3(0, 0, distance);
 	}
 }
+
+
+//#pragma strict
+//
+//var target:Transform;
+//var distance:float;
+//var lift:float;
+//
+//function Update () {
+//	//Debug.Log("Hello?");
+//	//Debug.Log("addition = "+ Vector3(0, lift, distance));
+//	transform.position = target.position + Vector3(0, lift, distance);
+//	
+//	//Debug.Log("pos = " +transform.position);
+//	//transform.LookAt(target);
+//	//transform.Rotate(Vector3(0,0,1), 180);
+//}
---
+++ Assets/MichaelPattieScripts/Balloon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balloon : MonoBehaviour {
+
+	public float inflatCoefficient;
+	public float deflateCoefficient;
+	public float airContent = 0.1f;
+	public float maxHeight;
+	public float minHeight;
+	public float maxDelta;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position+=new Vector3((airContent)*maxDelta,0,0);
+		deflate();
+
+		if(transform.position.y>maxHeight)
+			transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
+
+		if(transform.position.y<minHeight)
+			transform.position = new Vector3(transform.position.x, minHeight, transform.position.z);
+	}
+
+	public void inflate(){
+		airContent += inflatCoefficient;
+		if(airContent>1.0f)
+			airContent = 1.0f;
+	}
+
+	private void deflate(){
+		airContent -= deflateCoefficient;
+		if(airContent<-1.0f)
+			airContent = -1.0f;
+	}
+	
+}
