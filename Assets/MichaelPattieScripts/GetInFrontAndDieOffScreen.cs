c86f9c3fccf5398f663811f1907d1c8b4c21aeec
Michael Pattie <mjpattie@gmail.com>
2015-12-13 15:07:05 -0800

    Fixed level loading.

---
+++ Assets/MichaelPattieScripts/GetInFrontAndDieOffScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetInFrontAndDieOffScreen : MonoBehaviour {
+
+
+	private AddLevel manager;
+	private Transform Player;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<AddLevel>();
+		Player = GameObject.FindGameObjectWithTag("Player").transform;
+		transform.position = new Vector3(manager.getNextScenePosition(),0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Player.position.x - transform.position.x > manager.sceneWidth)
+			Destroy(gameObject);
+	}
+}
