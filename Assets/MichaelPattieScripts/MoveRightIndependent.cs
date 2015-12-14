7c3102bdc2c2aed428d839e361a82245f7c846c0
Brendan Keating <Brendan Keating>
2015-12-14 13:43:03 -0800

    Affected background and bullet changes. Immplemented music

--- Assets/MichaelPattieScripts/MoveRightIndependent.cs
+++ Assets/MichaelPattieScripts/MoveRightIndependent.cs
@@ -5,6 +5,10 @@ public class MoveRightIndependent : MonoBehaviour {
 
 	public float speed;
 	// Update is called once per frame
+	void Start ()
+	{
+		transform.Rotate (0, 90f, 90f);
+	}
 	void Update () {
 		transform.position+=new Vector3(Time.deltaTime*speed, 0, 0);
 	}
