a4fc52e01f717030191d9578f72feb8a2e4daafb
Michael Pattie <mjpattie@gmail.com>
2015-12-13 17:03:50 -0800

    Committing for pull.

--- Assets/MichaelPattieScripts/ProcessCollision.cs
+++ Assets/MichaelPattieScripts/ProcessCollision.cs
@@ -21,14 +21,33 @@ public class ProcessCollision : MonoBehaviour {
 		}
 	}
 
+
+	[HideInInspector]public bool speedPowerOn;
+	[HideInInspector]public bool inflatCoefficientPowerOn;
+	[HideInInspector]public bool deflateCoefficientPowerOn;
+	[HideInInspector]public bool speedUpPowerOn;
+	[HideInInspector]public bool slowDownPowerOn;
+	[HideInInspector]public bool immunityOn;
 	void applyCollisionResults(string colTag){
-//		switch(colTag){
+		switch(colTag){
+		case "Sandbag":
+			data.cancelPowerups();
+			data.deflateCoefficientPowerOn = true;
+			break;
 //		case "Sandbag":
 //			data.cancelPowerups();
 //			data.deflatePower = true;
 //			break;
-//		default:
+//		case "Sandbag":
+//			data.cancelPowerups();
+//			data.deflatePower = true;
 //			break;
-//		}
+//		case "Sandbag":
+//			data.cancelPowerups();
+//			data.deflatePower = true;
+//			break;
+		default:
+			break;
+		}
 	}
 }
