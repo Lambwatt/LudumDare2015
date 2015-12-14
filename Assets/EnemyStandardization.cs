a48c3ca850beef898374d3ab65860c37dc0534f9
Brendan Keating <Brendan Keating>
2015-12-14 11:33:24 -0800

    Added to code to standardize Z axis. Doesn't quite work yet.

---
+++ Assets/EnemyStandardization.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStandardization : MonoBehaviour {
+	public GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		enemies = GameObject.FindGameObjectsWithTag ("enemies");
+		foreach (GameObject enemy in enemies) 
+		{
+			float PosX = enemy.transform.position.x;
+			float PosY = enemy.transform.position.y;
+			enemy.transform.position = new Vector3(PosX, PosY, 0);
+		}
+	}
+}
